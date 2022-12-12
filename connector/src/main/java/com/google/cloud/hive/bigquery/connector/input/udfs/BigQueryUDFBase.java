7246bf7381b18c5dddad6e9ff24552267a32b343
Julien Phalip <jphalip@gmail.com>
2022-12-12 13:57:35 -0800

    Add BigQueryUDFBase parent class

--- connector/src/main/java/com/google/cloud/hive/bigquery/connector/input/udfs/BigQueryUDFBase.java
+++ connector/src/main/java/com/google/cloud/hive/bigquery/connector/input/udfs/BigQueryUDFBase.java
@@ -15,13 +15,26 @@
  */
 package com.google.cloud.hive.bigquery.connector.input.udfs;
 
-/** Converts Hive's modulo operator operator to BigQuery's mod() function. */
-public class BigQueryUDFRegExpContains extends BigQueryUDFBase {
+import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
 
-  public BigQueryUDFRegExpContains() {}
+/**
+ * Base class for the translation of Hive's built-in UDFs to the equivalent BigQuery SQL functions
+ * and operators.
+ */
+public abstract class BigQueryUDFBase extends GenericUDF {
 
   @Override
-  public String getDisplayString(String[] children) {
-    return String.format("REGEXP_CONTAINS(%s, r%s)", children[0], children[1]);
+  public ObjectInspector initialize(ObjectInspector[] arguments) throws UDFArgumentException {
+    // Ignore
+    return null;
+  }
+
+  @Override
+  public Object evaluate(DeferredObject[] arguments) throws HiveException {
+    // Ignore
+    return null;
   }
 }
--- connector/src/main/java/com/google/cloud/hive/bigquery/connector/input/udfs/BigQueryUDFMod.java
+++ connector/src/main/java/com/google/cloud/hive/bigquery/connector/input/udfs/BigQueryUDFMod.java
@@ -15,27 +15,10 @@
  */
 package com.google.cloud.hive.bigquery.connector.input.udfs;
 
-import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
-import org.apache.hadoop.hive.ql.metadata.HiveException;
-import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
-import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
-
 /** Converts Hive's modulo operator operator to BigQuery's mod() function. */
-public class BigQueryUDFMod extends GenericUDF {
+public class BigQueryUDFMod extends BigQueryUDFBase {
 
   public BigQueryUDFMod() {}
-
-  @Override
-  public ObjectInspector initialize(ObjectInspector[] arguments) throws UDFArgumentException {
-    // Ignore
-    return null;
-  }
-
-  @Override
-  public Object evaluate(DeferredObject[] arguments) throws HiveException {
-    // Ignore
-    return null;
-  }
 
   @Override
   public String getDisplayString(String[] children) {
