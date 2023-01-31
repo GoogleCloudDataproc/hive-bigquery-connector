3d53c0ce4c5259c8b704f9356dd2e1b7ca472b08
Julien Phalip <jphalip@gmail.com>
2023-01-30 16:30:55 -0800

    Fix ArrowSerializer

--- connector/src/main/java/com/google/cloud/hive/bigquery/connector/input/arrow/ArrowSerializer.java
+++ connector/src/main/java/com/google/cloud/hive/bigquery/connector/input/arrow/ArrowSerializer.java
@@ -33,8 +33,6 @@ import org.apache.hadoop.io.*;
 import repackaged.by.hivebqconnector.org.apache.arrow.vector.*;
 import repackaged.by.hivebqconnector.org.apache.arrow.vector.complex.ListVector;
 import repackaged.by.hivebqconnector.org.apache.arrow.vector.complex.StructVector;
-import repackaged.by.hivebqconnector.org.apache.arrow.vector.util.JsonStringArrayList;
-import repackaged.by.hivebqconnector.org.apache.arrow.vector.util.JsonStringHashMap;
 
 public class ArrowSerializer {
 
@@ -48,82 +46,35 @@ public class ArrowSerializer {
     }
 
     if (objectInspector instanceof BooleanObjectInspector) {
-      boolean booleanValue;
-      if (value instanceof BitVector) {
-        booleanValue = ((BitVector) value).get(rowId) == 1;
-      } else {
-        booleanValue = (boolean) value;
-      }
-      return new BooleanWritable(booleanValue);
+      return new BooleanWritable(((BitVector) value).get(rowId) == 1);
     }
 
     if (objectInspector instanceof ByteObjectInspector) { // Tiny Int
-      byte byteValue;
-      if (value instanceof BigIntVector) {
-        byteValue = (byte) ((BigIntVector) value).get(rowId);
-      } else {
-        byteValue = ((Long) value).byteValue();
-      }
-      return new ByteWritable(byteValue);
+      return new ByteWritable((byte) ((BigIntVector) value).get(rowId));
     }
 
     if (objectInspector instanceof ShortObjectInspector) { // Small Int
-      short shortValue;
-      if (value instanceof BigIntVector) {
-        shortValue = (short) ((BigIntVector) value).get(rowId);
-      } else {
-        shortValue = ((Long) value).shortValue();
-      }
-      return new ShortWritable(shortValue);
+      return new ShortWritable((short) ((BigIntVector) value).get(rowId));
     }
 
     if (objectInspector instanceof IntObjectInspector) { // Regular Int
-      int intValue;
-      if (value instanceof BigIntVector) {
-        intValue = (int) ((BigIntVector) value).get(rowId);
-      } else {
-        intValue = ((Long) value).intValue();
-      }
-      return new IntWritable(intValue);
+      return new IntWritable((int) ((BigIntVector) value).get(rowId));
     }
 
     if (objectInspector instanceof LongObjectInspector) { // Big Int
-      long longValue;
-      if (value instanceof BigIntVector) {
-        longValue = ((BigIntVector) value).get(rowId);
-      } else {
-        longValue = (long) value;
-      }
-      return new LongWritable(longValue);
+      return new LongWritable(((BigIntVector) value).get(rowId));
     }
 
     if (objectInspector instanceof FloatObjectInspector) {
-      float floatValue;
-      if (value instanceof Float8Vector) {
-        floatValue = (float) ((Float8Vector) value).get(rowId);
-      } else {
-        floatValue = ((Double) value).floatValue();
-      }
-      return new FloatWritable(floatValue);
+      return new FloatWritable((float) ((Float8Vector) value).get(rowId));
     }
 
     if (objectInspector instanceof DoubleObjectInspector) {
-      double doubleValue;
-      if (value instanceof Float8Vector) {
-        doubleValue = ((Float8Vector) value).get(rowId);
-      } else {
-        doubleValue = (double) value;
-      }
-      return new DoubleWritable(doubleValue);
+      return new DoubleWritable(((Float8Vector) value).get(rowId));
     }
 
     if (objectInspector instanceof HiveDecimalObjectInspector) {
-      BigDecimal decimalValue;
-      if (value instanceof DecimalVector) {
-        decimalValue = ((DecimalVector) value).getObject(rowId);
-      } else {
-        decimalValue = (BigDecimal) value;
-      }
+      BigDecimal decimalValue = ((DecimalVector) value).getObject(rowId);
       HiveDecimal hiveDecimal = HiveDecimal.create(decimalValue);
       HiveDecimal.enforcePrecisionScale(
           hiveDecimal, decimalValue.precision(), decimalValue.scale());
@@ -133,36 +84,20 @@ public class ArrowSerializer {
     if (objectInspector instanceof StringObjectInspector
         || objectInspector instanceof HiveVarcharObjectInspector
         || objectInspector instanceof HiveCharObjectInspector) {
-      if (value instanceof VarCharVector) {
-        VarCharVector v = (VarCharVector) value;
-        if (v.isSet(rowId) == 0) {
-          return null;
-        } else {
-          return new Text(v.getObject(rowId).toString());
-        }
+      VarCharVector v = (VarCharVector) value;
+      if (v.isSet(rowId) == 0) {
+        return null;
       } else {
-        return new Text(value.toString());
+        return new Text(v.getObject(rowId).toString());
       }
     }
 
     if (objectInspector instanceof BinaryObjectInspector) {
-      byte[] byteArray;
-      if (value instanceof VarBinaryVector) {
-        byteArray = ((VarBinaryVector) value).getObject(rowId);
-      } else {
-        byteArray = (byte[]) value;
-      }
-      return new BytesWritable(byteArray);
+      return new BytesWritable(((VarBinaryVector) value).getObject(rowId));
     }
 
     if (objectInspector instanceof DateObjectInspector) {
-      int intValue;
-      if (value instanceof DateDayVector) {
-        intValue = ((DateDayVector) value).get(rowId);
-      } else {
-        intValue = (int) value;
-      }
-      LocalDate localDate = LocalDate.ofEpochDay(intValue);
+      LocalDate localDate = LocalDate.ofEpochDay(((DateDayVector) value).get(rowId));
       Date date = new Date();
       date.setDayOfMonth(localDate.getDayOfMonth());
       date.setMonth(localDate.getMonth().getValue());
@@ -190,19 +125,14 @@ public class ArrowSerializer {
 
     if (objectInspector instanceof ListObjectInspector) { // Array/List type
       ListObjectInspector loi = (ListObjectInspector) objectInspector;
-      JsonStringArrayList<?> listData;
-      if (value instanceof ListVector) {
-        ListVector listVector = (ListVector) value;
-        listData = (JsonStringArrayList<?>) listVector.getObject(rowId);
-      } else if (value instanceof JsonStringArrayList) {
-        listData = (JsonStringArrayList) value;
-      } else {
-        throw new RuntimeException("Unexpected list type:" + value.getClass().getName());
-      }
-      int numItems = listData.size();
+      ListVector listVector = (ListVector) value;
+      int start = listVector.getOffsetBuffer().getInt(rowId * 4L);
+      int end = listVector.getOffsetBuffer().getInt((rowId + 1) * 4L);
+      int numItems = end - start;
       Object[] children = new Object[numItems];
       for (int i = 0; i < numItems; i++) {
-        children[i] = serialize(listData.get(i), loi.getListElementObjectInspector(), i);
+        children[i] =
+            serialize(listVector.getDataVector(), loi.getListElementObjectInspector(), start + i);
       }
       return children;
     }
@@ -210,54 +140,32 @@ public class ArrowSerializer {
     if (objectInspector instanceof MapObjectInspector) { // Map type
       MapObjectInspector moi = (MapObjectInspector) objectInspector;
       Map<Object, Object> map = new HashMap<>();
-      JsonStringArrayList<Map<?, ?>> list;
-      if (value instanceof JsonStringArrayList) {
-        list = (JsonStringArrayList<Map<?, ?>>) value;
-      } else if (value instanceof ListVector) {
-        ListVector listVector = (ListVector) value;
-        list = (JsonStringArrayList<Map<?, ?>>) listVector.getObject(rowId);
-      } else {
-        throw new RuntimeException("Unexpected map type:" + value.getClass().getName());
+      ListVector listVector = (ListVector) value;
+      StructVector structVector = (StructVector) listVector.getDataVector();
+      int start = listVector.getOffsetBuffer().getInt(rowId * 4L);
+      int end = listVector.getOffsetBuffer().getInt((rowId + 1) * 4L);
+      ValueVector keys =
+          structVector.getChildVectorWithOrdinal(KeyValueObjectInspector.KEY_FIELD_NAME).vector;
+      ValueVector values =
+          structVector.getChildVectorWithOrdinal(KeyValueObjectInspector.VALUE_FIELD_NAME).vector;
+      int numItems = end - start;
+      for (int i = 0; i < numItems; i++) {
+        Object k = serialize(keys, moi.getMapKeyObjectInspector(), start + i);
+        Object v = serialize(values, moi.getMapValueObjectInspector(), start + i);
+        map.put(k, v);
       }
-      list.forEach(
-          item -> {
-            Object k =
-                serialize(
-                    item.get(KeyValueObjectInspector.KEY_FIELD_NAME),
-                    moi.getMapKeyObjectInspector(),
-                    -1 /* rowId will be ignored in this case */);
-            Object v =
-                serialize(
-                    item.get(KeyValueObjectInspector.VALUE_FIELD_NAME),
-                    moi.getMapValueObjectInspector(),
-                    -1 /* rowId will be ignored in this case */);
-            map.put(k, v);
-          });
       return map;
     }
 
     if (objectInspector instanceof StructObjectInspector) { // Record/Struct type
       StructObjectInspector soi = (StructObjectInspector) objectInspector;
-      JsonStringHashMap<?, ?> structData;
-      if (value instanceof StructVector) {
-        StructVector structVector = (StructVector) value;
-        structData = (JsonStringHashMap<?, ?>) (structVector).getObject(rowId);
-      } else if (value instanceof JsonStringHashMap) {
-        structData = (JsonStringHashMap<?, ?>) value;
-      } else {
-        throw new RuntimeException("Unexpected struct type:" + value.getClass().getName());
-      }
-      int numFields = structData.size();
-      Object[] row = new Object[numFields];
+      List<FieldVector> fieldVectors = ((StructVector) value).getChildrenFromFields();
+      Object[] row = new Object[fieldVectors.size()];
       int i = 0;
-      for (Map.Entry<?, ?> entry : structData.entrySet()) {
-        String fieldName = (String) entry.getKey();
-        Object fieldValue = entry.getValue();
+      for (FieldVector fieldVector : fieldVectors) {
         row[i] =
             serialize(
-                fieldValue,
-                soi.getStructFieldRef(fieldName).getFieldObjectInspector(),
-                -1 /* rowId will be ignored in this case */);
+                fieldVector, soi.getAllStructFieldRefs().get(i).getFieldObjectInspector(), rowId);
         i++;
       }
       return row;
