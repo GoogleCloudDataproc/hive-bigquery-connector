43b522abed6cacd2107e10c1e5c97222cf6acecd
Dagang Wei <functicons@gmail.com>
2023-03-23 17:35:18 -0700

    Move integration tests to integration/ folder (#49)

--- connector/src/test/java/com/google/cloud/hive/bigquery/connector/integration/BigLakeIntegrationTests.java
+++ connector/src/test/java/com/google/cloud/hive/bigquery/connector/integration/BigLakeIntegrationTests.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.google.cloud.hive.bigquery.connector;
+package com.google.cloud.hive.bigquery.connector.integration;
 
 import static com.google.cloud.hive.bigquery.connector.TestUtils.*;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
