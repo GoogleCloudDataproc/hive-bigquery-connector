443df2d0c35ef54386fffedc204bfed84d4ac45f
Yi Zhang <yigress@google.com>
2023-06-06 17:22:25 -0700

    Reuse BigqueryClientFactory for write

--- connector/src/main/java/com/google/cloud/hive/bigquery/connector/output/direct/DirectUtils.java
+++ connector/src/main/java/com/google/cloud/hive/bigquery/connector/output/direct/DirectUtils.java
@@ -24,12 +24,33 @@ import com.google.cloud.bigquery.connector.common.BigQueryDirectDataWriterHelper
 import com.google.cloud.bigquery.storage.v1.ProtoSchema;
 import com.google.cloud.hive.bigquery.connector.config.HiveBigQueryConfig;
 import com.google.cloud.hive.bigquery.connector.config.HiveBigQueryConnectorModule;
+import com.google.common.base.Optional;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import java.util.Properties;
 import org.apache.hadoop.mapred.JobConf;
 
 public class DirectUtils {
+
+  // Share same BigQueryClientFactory for same JVM
+  public static BigQueryClientFactory bigqueryWriteClientFactory;
+
+  public static BigQueryClientFactory getOrCreateWriteClientFactory(
+      JobConf jobConf, Properties tableProperties) {
+    if (bigqueryWriteClientFactory == null) {
+      Injector injector =
+          Guice.createInjector(
+              new BigQueryClientModule(),
+              new HiveBigQueryConnectorModule(jobConf, tableProperties));
+      HeaderProvider headerProvider = injector.getInstance(HeaderProvider.class);
+      BigQueryCredentialsSupplier credentialsSupplier =
+          injector.getInstance(BigQueryCredentialsSupplier.class);
+      HiveBigQueryConfig opts = injector.getInstance(HiveBigQueryConfig.class);
+      bigqueryWriteClientFactory =
+          new BigQueryClientFactory(credentialsSupplier, headerProvider, opts);
+    }
+    return bigqueryWriteClientFactory;
+  }
 
   /**
    * Instantiates a BigQueryDirectDataWriterHelper object from the bigquery-connector-common
@@ -38,24 +59,17 @@ public class DirectUtils {
    */
   public static BigQueryDirectDataWriterHelper createStreamWriter(
       JobConf jobConf, TableId tableId, Properties tableProperties, ProtoSchema schema) {
-    Injector injector =
-        Guice.createInjector(
-            new BigQueryClientModule(), new HiveBigQueryConnectorModule(jobConf, tableProperties));
-    HeaderProvider headerProvider = injector.getInstance(HeaderProvider.class);
-    BigQueryCredentialsSupplier credentialsSupplier =
-        injector.getInstance(BigQueryCredentialsSupplier.class);
-    HiveBigQueryConfig opts = injector.getInstance(HiveBigQueryConfig.class);
+    BigQueryClientFactory writeClientFactory =
+        getOrCreateWriteClientFactory(jobConf, tableProperties);
     String tablePath =
         String.format(
             "projects/%s/datasets/%s/tables/%s",
             tableId.getProject(), tableId.getDataset(), tableId.getTable());
-    BigQueryClientFactory writeClientFactory =
-        new BigQueryClientFactory(credentialsSupplier, headerProvider, opts);
     return new BigQueryDirectDataWriterHelper(
         writeClientFactory,
         tablePath,
         schema,
-        opts.getBigQueryClientRetrySettings(),
-        opts.getTraceId());
+        HiveBigQueryConfig.getDefaultBigQueryClientRetrySettings(),
+        Optional.of(HiveBigQueryConfig.getTraceId(jobConf)));
   }
 }
